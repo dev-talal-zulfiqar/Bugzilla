482978a9c2ccd5621f991c3b8e59465e3631bb35
Talal-Z <talal.zulfiqar@devsinc.com>
2022-02-21 05:52:34 -0500

    project-module: Crud operations project in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :projects_users
+  has_many :projects, through: :projects_users
+
+  has_many :projects, :foreign_key => 'created_by', :class_name => 'Project'
+
+  enum role: { manager: 0, developer: 1, software_quality_assurance: 2 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
