1a847112cc507a885dab0698491c94242e6fe51c
Talal-Z <talal.zulfiqar@devsinc.com>
2022-02-23 09:46:17 -0500

    Bug module: worked on CRUD operations and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,11 +8,8 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
-    if authorize @project
-      @user = User.where(role: ['developer','software_quality_assurance'])
-    else
-      access_denied
-    end
+    authorize @project
+    @user = User.where(role: ['developer','software_quality_assurance'])
   end
 
   def create
@@ -49,7 +46,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params.require(:id))
     @user = @project.users
-    authorize @project, :show?
+    authorize @project
     @project
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@
 class User < ApplicationRecord
   has_many :created_bugs, class_name: 'Bug', foreign_key: :created_by_id
   has_many :assigned_bugs, class_name: 'Bug', foreign_key: :assigned_to_id
-
   has_many :projects_users
   has_many :projects, through: :projects_users
-
   has_many :projects, foreign_key: :created_by, class_name: :Project
 
   enum role: { manager: 0, developer: 1, software_quality_assurance: 2 }
