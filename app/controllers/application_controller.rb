718ac90ec0956982d5c0c1c6b49081b23ff3cf73
Talal-Z <talal.zulfiqar@devsinc.com>
2022-02-28 14:51:58 +0500

    application controller : record or page not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
+# Application controller Home
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   protected
 
   def configure_permitted_parameters
@@ -9,7 +14,11 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
+  end
+
+  def record_not_found
+    render text: '404 Not Found', status: 404
   end
 end
